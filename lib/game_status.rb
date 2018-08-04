28f539e2ba9f0519941be46971652806ffdc3661
Amin Ullah <aullah@umich.edu>
2018-08-04 00:04:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,9 +6,9 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5]  # Middle row
-  [6,7,8]  #Bottom row
-  [0,4,8]  #left-right diagonal
+  [3,4,5],  # Middle row
+  [6,7,8],  #Bottom row
+  [0,4,8],  #left-right diagonal
   [6,4,2]  #right-left diagonal
 ]
 
